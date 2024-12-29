c34f6c57f70d630332d206148f31740c38da7d78
Matthew Schilling <77081591+mschilling314@users.noreply.github.com>
2024-12-29 12:18:39 -0600

    Create graphing.py

---
+++ graphing.py
@@ -0,0 +1,56 @@
+import matplotlib.pyplot as plt
+import sqlite3
+import pandas as pd
+import pandasql as ps
+
+from DataClasses.Unit import Unit
+from DataClasses.FloorPlan import FloorPlan
+
+
+def get_table_df(connection: sqlite3.Connection, table: str) -> pd.DataFrame:
+    sql_query = f"SELECT * FROM {table}"
+    return pd.read_sql_query(sql = sql_query, con = connection)
+
+
+def get_average_rent_df_for_floor_plans(floor_plans: pd.DataFrame, units: pd.DataFrame) -> pd.DataFrame:
+    query = """
+            SELECT units.date_of_update AS date, floor_plans.name, AVG(units.price) as price
+            FROM units
+            JOIN floor_plans
+            ON units.floor_plan_id = floor_plans.floor_plan_id
+            GROUP BY units.date_of_update, units.floor_plan_id
+            """
+    return ps.sqldf(query, locals())
+
+
+def graph_price_per_day(df: pd.DataFrame, floor_plan_name: str="A21", xaxis: str="Date", yaxis: str="Price", title: str="Price per Day") -> None:
+    """
+    Graphs the price given on a daily basis.
+
+    Inputs:
+    ------
+    df: A Pandas DataFrame that MUST have "date" and "price" columns.
+    """
+    df_to_plot = df[df["name"] == floor_plan_name]
+    plt.plot(df_to_plot["date"], df_to_plot["price"])
+
+    plt.xlabel(xaxis)
+    plt.ylabel(yaxis)
+    plt.title(title)
+
+    plt.show()
+
+def print_price_per_day_for_floor_plan(df: pd.DataFrame, floor_plan_name: str) -> None:
+    print(df[df["name"] == floor_plan_name])
+
+
+if __name__=="__main__":
+    db_path = "data/wpe.sqlite"
+    conn = sqlite3.connect(db_path)
+    floor_plans = get_table_df(conn, "FloorPlans")
+    units = get_table_df(conn, "Units")
+    
+    average_rent_by_floor_plan = get_average_rent_df_for_floor_plans(floor_plans=floor_plans, units=units)
+    
+    print_price_per_day_for_floor_plan(average_rent_by_floor_plan, "A+D-2")
+    graph_price_per_day(average_rent_by_floor_plan, yaxis="Average Price", floor_plan_name="A+D-2")
