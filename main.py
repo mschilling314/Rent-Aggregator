ff0ecbe3ef267d740881c205d1be2fd80c4dfebd
Matthew Schilling <77081591+mschilling314@users.noreply.github.com>
2024-12-29 13:34:07 -0600

    Refactor main, write test

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
 import WPE.wpescraper as wpescraper
 
 
-if __name__=="__main__":
+def main():
+    """
+    Function executed as primary entrypoint into program.
+    """
     wpescraper.scrape()
+
+
+if __name__=="__main__":
+    main()
