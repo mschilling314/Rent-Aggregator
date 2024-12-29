310679b0473c76dcc79f3425fbabde2518c5e1dc
Matthew Schilling <77081591+mschilling314@users.noreply.github.com>
2024-12-29 12:22:59 -0600

    Refactor into plotting module

--- graphing.py
+++ graphing.py
@@ -4,24 +4,8 @@ import pandas as pd
 import pandasql as ps
 
 from analysis.analyze import get_table_df, get_average_rent_df_for_floor_plans
+from plotting.plot import graph_price_per_day
 
-
-def graph_price_per_day(df: pd.DataFrame, floor_plan_name: str="A21", xaxis: str="Date", yaxis: str="Price", title: str="Price per Day") -> None:
-    """
-    Graphs the price given on a daily basis.
-
-    Inputs:
-    ------
-    df: A Pandas DataFrame that MUST have "date" and "price" columns.
-    """
-    df_to_plot = df[df["name"] == floor_plan_name]
-    plt.plot(df_to_plot["date"], df_to_plot["price"])
-
-    plt.xlabel(xaxis)
-    plt.ylabel(yaxis)
-    plt.title(title)
-
-    plt.show()
 
 def print_price_per_day_for_floor_plan(df: pd.DataFrame, floor_plan_name: str) -> None:
     print(df[df["name"] == floor_plan_name])
---
+++ plotting/plot.py
@@ -0,0 +1,19 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+
+def graph_price_per_day(df: pd.DataFrame, floor_plan_name: str="A21", xaxis: str="Date", yaxis: str="Price", title: str="Price per Day") -> None:
+    """
+    Graphs the price given on a daily basis.
+
+    Inputs:
+    ------
+    df: A Pandas DataFrame that MUST have "date" and "price" columns.
+    """
+    df_to_plot = df[df["name"] == floor_plan_name]
+    plt.plot(df_to_plot["date"], df_to_plot["price"])
+
+    plt.xlabel(xaxis)
+    plt.ylabel(yaxis)
+    plt.title(title)
+
+    plt.show()
