b9b6f06f7a50accf7cead8ca84953a80a7c84f4c
Matthew Schilling <77081591+mschilling314@users.noreply.github.com>
2024-12-29 13:49:06 -0600

    Add logging to main.

--- main.py
+++ main.py
@@ -1,11 +1,21 @@
+import logging
+
 import WPE.wpescraper as wpescraper
+
+logging.basicConfig(
+    level=logging.INFO,  # Set minimum logging level to INFO
+    format="%(asctime)s - %(levelname)s - %(message)s",  # Log format
+    handlers=[logging.FileHandler("logs/app.log", mode="a"), logging.StreamHandler()]  # Output to both a file and the console
+)
 
 
 def main():
     """
     Function executed as primary entrypoint into program.
     """
+    logging.info("Beginning execution of the app.")
     wpescraper.scrape()
+    logging.info("Completed execution of the app.")
 
 
 if __name__=="__main__":
